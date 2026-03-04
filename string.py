81a8945fef01469c1cfe5a4f9ea230c6865b2710
NivedithaChandran <nivedithachandran45@gmail.com>
2026-03-04 15:50:59 +0530

    added line

--- string.py
+++ string.py
@@ -1,6 +1,7 @@
-str=input("enter a string:")
+string=input("enter a string:")
 str2=str[::-1]
-if str==str2:
+print("hello world")
+if string==str2:
     print("palindrome")
 else:
     print("not a palindrome")
