685b6ac22b35049aea300065abb18273e5ef059f
NivedithaChandran <nivedithachandran45@gmail.com>
2026-03-07 10:45:09 +0530

    added addition function

---
+++ c-programs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    float x,y;
+    do
+  {
+    char op;
+    printf("\nenter operation:");
+    scanf("%c",&op);
+    switch(op)
+     {
+         case 'a':
+              printf("enter 1st number:");
+              scanf("%f",&x);
+              printf("enter 2nd number:");
+              scanf("%f",&y);
+              printf("\n %.2f",(x+y));
+              break;
+         default:
+              printf("exit");
+              break;
+        
+     }
+   }while(x!=0);
+
+
+return 0;
+}
--- string.py
+++
@@ -1,7 +0,0 @@
-string=input("enter a string:")
-str2=str[::-1]
-print("hello world")
-if string==str2:
-    print("palindrome")
-else:
-    print("not a palindrome")
